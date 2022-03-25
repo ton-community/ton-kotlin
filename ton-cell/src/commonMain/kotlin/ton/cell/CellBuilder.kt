c314d6a0a518db6db72ae3b19fbbd216eda1b566
Andrey Pfau <a.pfau@adguard.com>
2022-03-25 23:02:17 +0300

    cell slices

--- ton-cell/src/commonMain/kotlin/ton/cell/CellBuilder.kt
+++ ton-cell/src/commonMain/kotlin/ton/cell/CellBuilder.kt
@@ -3,17 +3,25 @@ package ton.cell
 import ton.bitstring.BitStringBuilder
 import ton.bitstring.BitStringWriter
 
-class CellBuilder(
+interface CellBuilder : CellWriter {
+    override var writePosition: Int
+
+    fun toCell(): Cell
+}
+
+private class CellBuilderImpl(
     private val data: BitStringBuilder = BitStringBuilder(),
     override var cellReferences: MutableList<Cell> = ArrayList(),
-) : CellWriter, BitStringWriter by data {
+) : CellBuilder, BitStringWriter by data {
     override var writePosition: Int
         get() = data.writePosition
         set(value) {
             data.writePosition = value
         }
 
-    fun toCell() = Cell(data.toBitString(), cellReferences)
+    override fun toCell() = Cell(data.toBitString(), cellReferences)
 }
+
+fun CellBuilder(): CellBuilder = CellBuilderImpl()
 
 fun buildCell(builder: CellBuilder.() -> Unit): Cell = CellBuilder().apply(builder).toCell()
