8890a890b1e3737bc0a30bdbe8042a61862f0a95
Andrey Pfau <andreypfau@yandex.ru>
2022-04-16 21:35:22 +0300

    move fift to it own branch

--- ton-common-biginteger/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-kotlin {
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-            }
-        }
-    }
-}
