2ea6291be203ec56abc125e33d797597cd5414cd
Andrey Pfau <andreypfau@ton.org>
2022-05-09 16:19:23 +0300

    fix fift

---
+++ ton-fift/src/commonMain/kotlin/org/ton/fift/interpretator.kt
@@ -0,0 +1,191 @@
+package org.ton.fift
+
+import org.ton.types.ExceptionCode
+import org.ton.types.int257.Int257
+import org.ton.types.int257.int257
+
+class FiftInterpretator(
+        val stack: Stack = Stack(),
+        val dictionary: Dictionary = Dictionary().apply { defineBasicWords() },
+        var output: (String) -> Unit = { print(it) },
+) {
+
+    var state: Int = 0
+    var charPos = 0
+    var currentLine: String = ""
+
+    fun interpret(line: String) = try {
+        currentLine = line
+        charPos = 0
+
+        while (canRead()) {
+            skipSpace()
+            val pos = charPos
+            var word = scanWord(' ')
+            var wordDef = dictionary[word]
+
+            if (wordDef == null) {
+                wordDef = dictionary["$word "]
+                if (wordDef != null) {
+                    charPos++
+                }
+            }
+
+            if (wordDef == null) {
+                charPos = pos
+                val wordBuilder = StringBuilder()
+                while (canRead()) {
+                    val char = currentLine[charPos++]
+                    if (char != ' ' && char != '\t') {
+                        wordBuilder.append(char)
+
+                        val currentWord = wordBuilder.toString()
+                        wordDef = dictionary[currentWord]
+                        if (wordDef != null) {
+                            word = currentWord
+                            break
+                        }
+                    }
+                }
+                if (wordDef == null) {
+                    charPos = pos
+                }
+            }
+
+            if (wordDef != null) {
+                interpret(wordDef)
+            } else {
+                charPos += word.length
+                val int257s = word.parseInt256()
+                when (int257s.size) {
+                    1 -> {
+                        stack.push(int257s.first())
+                        stack.pushArgCount(1)
+                    }
+                    2 -> {
+                        stack.push(int257s[0])
+                        stack.push(int257s[1])
+                        stack.pushArgCount(2)
+                    }
+                    else -> throw FiftException(ExceptionCode.DictionaryError, "Unknown word: $word")
+                }
+                compileExecute()
+            }
+        }
+        output(" ok\n")
+    } catch (e: FiftException) {
+        stack.clear()
+        state = 0
+        throw e
+    }
+
+    fun interpret(wordDef: WordDef) {
+        if (wordDef.isActive) {
+            wordDef.execute(this)
+            compileExecute()
+        } else {
+            stack.push(0)
+            stack.push(wordDef)
+            compileExecute()
+        }
+    }
+
+    fun compileExecute() {
+        if (state > 0) {
+            interpretCompileInternal()
+        } else {
+            interpretExecuteInternal()
+        }
+    }
+
+    fun canRead() = charPos in 0..currentLine.lastIndex
+
+    fun scanWord(separator: Char = ' ', readSeparator: Boolean = false): String {
+        val wordBuilder = StringBuilder()
+        while (canRead()) {
+            val char = currentLine[charPos]
+            if (char != separator) {
+                charPos++
+                wordBuilder.append(char)
+            } else {
+                if (readSeparator) {
+                    charPos++
+                }
+                break
+            }
+        }
+        return wordBuilder.toString()
+    }
+
+    fun skipSpace() {
+        while (canRead()) {
+            val char = currentLine[charPos]
+            if (char == ' ' || char == '\t' || char == '\n') {
+                charPos++
+            } else {
+                break
+            }
+        }
+    }
+
+    fun String.parseInt256(): Array<Int257> = try {
+        val fracStrings = split('/')
+        when {
+            fracStrings.size == 1 -> arrayOf(int257(fracStrings.first()))
+            fracStrings.size > 1 -> arrayOf(int257(fracStrings.first()), int257(fracStrings[1]))
+            else -> emptyArray()
+        }
+    } catch (e: NumberFormatException) {
+        emptyArray()
+    }
+
+    operator fun invoke(block: FiftInterpretator.() -> Unit) = apply(block)
+
+    val String.utf8Code: Int
+        get() = when {
+            isNotEmpty() && get(0).code < 0x80 -> get(0).code
+            length >= 2
+                    && (get(0).code and 0xe0) == 0xc0
+                    && (get(1).code and 0xc0) == 0x80 ->
+                ((get(0).code and 0x1f) shl 6) or (get(1).code and 0x3f)
+            length >= 3
+                    && (get(0).code and 0xf0) == 0xe0
+                    && (get(1).code and 0xc0) == 0x80
+                    && (get(2).code and 0xc0) == 0x80 ->
+                ((get(0).code and 0x0f) shl 12) or ((get(1).code and 0x3f) shl 6) or (get(2).code and 0x3f)
+            length >= 4
+                    && (get(0).code and 0xf8) == 0xf0
+                    && (get(1).code and 0xc0) == 0x80
+                    && (get(2).code and 0xc0) == 0x80
+                    && (get(3).code and 0xc0) == 0x80 ->
+                ((get(0).code and 7) shl 18) or ((get(1).code and 0x3f) shl 12) or ((get(2).code and 0x3f) shl 6) or (get(
+                        3).code and 0x3f)
+            else -> -1
+        }
+
+    val Int.utf8Char: String
+        get() = when {
+            this < 0x80 -> toChar().toString()
+            this < 0x800 -> charArrayOf(
+                    (0xc0 + (this shr 6)).toChar(),
+                    (0x80 + (this and 0x3f)).toChar(),
+            ).concatToString()
+            this < 0x10000 -> charArrayOf(
+                    (0xe0 + (this shr 12)).toChar(),
+                    (0x80 + ((this shr 6) and 0x3f)).toChar(),
+                    (0x80 + (this and 0x3d)).toChar()
+            ).concatToString()
+            this < 0x200000 -> charArrayOf(
+                    (0xf0 + (this shr 18)).toChar(),
+                    (0x80 + ((this shr 12) and 0x3f)).toChar(),
+                    (0x80 + ((this shr 6) and 0x3f)).toChar(),
+                    (0x80 + (this and 0x3f)).toChar()
+            ).concatToString()
+            else -> ""
+        }
+}
+
+fun FiftInterpretator.checkCompile() = check(state > 0) { "Compilation mode only" }
+fun FiftInterpretator.checkExecute() = check(state > 0) { "Interpretation mode only" }
+fun FiftInterpretator.checkNotIntExec() = check(state >= 0) { "not allowed in internal interpret mode" }
+fun FiftInterpretator.checkIntExec() = check(state < 0) { "internal interpret mode only" }
---
+++ ton-fift/src/commonTest/kotlin/org/ton/fift/WordsTest.kt
@@ -0,0 +1,184 @@
+package org.ton.fift
+
+import io.ktor.utils.io.core.*
+import org.ton.types.int257.int257
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+fun testFift(block: FiftInterpretator.() -> Unit) {
+    val fift = FiftInterpretator(output = {})
+    fift.apply(block)
+    assertTrue(fift.stack.isEmpty)
+}
+
+fun main() {
+    val a = buildPacket {
+
+    }
+    a.readByte()
+}
+
+class WordsTest {
+    @Test
+    fun testBasicArithmetic() = testFift {
+        interpret("7 4 -")
+        assertEquals(int257(3), stack.popInt257())
+        interpret("2 3 4 * +")
+        interpret("2 3 + 4 *")
+        assertEquals(int257(20), stack.popInt257())
+        assertEquals(int257(14), stack.popInt257())
+    }
+
+
+    @Test
+    fun `test word '+ '`() = testFift {
+        interpret("1 2 +")
+        assertEquals(int257(3), stack.popInt257())
+        interpret("5 -10 +")
+        assertEquals(int257(-5), stack.popInt257())
+        interpret("42 37 94 + + 11 +")
+        assertEquals(int257(184), stack.popInt257())
+        assertTrue(stack.isEmpty)
+    }
+
+    @Test
+    fun `test word '- '`() = testFift {
+        interpret("5 3 -")
+        assertEquals(int257(2), stack.popInt257())
+        interpret("-10 -3 -")
+        assertEquals(int257(-7), stack.popInt257())
+        interpret("-19 397 -24 - - 33 -")
+        assertEquals(int257(-473), stack.popInt257())
+        assertTrue(stack.isEmpty)
+    }
+
+    @Test
+    fun `test word 'negate '`() = testFift {
+        interpret("53 negate")
+        assertEquals(int257(-53), stack.popInt257())
+        interpret("-10 negate")
+        assertEquals(int257(10), stack.popInt257())
+        interpret("397 -24 negate negate 99 negate 11")
+        assertEquals(int257(11), stack.popInt257())
+        assertEquals(int257(-99), stack.popInt257())
+        assertEquals(int257(-24), stack.popInt257())
+        assertEquals(int257(397), stack.popInt257())
+    }
+
+    @Test
+    fun testDup() = testFift {
+        interpret("1 2 dup")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+    }
+
+    @Test
+    fun testDrop() = testFift {
+        interpret("3 1 2 drop")
+        assertEquals(int257(1), stack.popInt257())
+        interpret("drop")
+    }
+
+    @Test
+    fun testSwap() = testFift {
+        interpret("1 2 3 swap")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(3), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+    }
+
+    @Test
+    fun testRot() = testFift {
+        interpret("1 2 3 rot")
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(3), stack.popInt257())
+        assertEquals(int257(2), stack.popInt257())
+    }
+
+    @Test
+    fun testNegRot() = testFift {
+        interpret("1 2 3 -rot")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(3), stack.popInt257())
+        interpret("1 2 3 rot rot")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(3), stack.popInt257())
+    }
+
+    @Test
+    fun testOver() = testFift {
+        interpret("1 2 over")
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+    }
+
+    @Test
+    fun testTuck() = testFift {
+        interpret("1 2 tuck")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(2), stack.popInt257())
+        interpret("1 2 swap over")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(2), stack.popInt257())
+    }
+
+    @Test
+    fun testNip() = testFift {
+        interpret("1 2 nip")
+        assertEquals(int257(2), stack.popInt257())
+        interpret("1 2 swap drop")
+        assertEquals(int257(2), stack.popInt257())
+    }
+
+    @Test
+    fun test2dup() = testFift {
+        interpret("1 2 2dup")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        interpret("1 2 over over")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+    }
+
+    @Test
+    fun test2drop() = testFift {
+        interpret("1 2 2drop")
+        assertTrue(stack.isEmpty)
+        interpret("1 2 drop drop")
+        assertTrue(stack.isEmpty)
+    }
+
+    @Test
+    fun test2swap() = testFift {
+        interpret("1 2 3 4 2swap")
+        assertEquals(int257(2), stack.popInt257())
+        assertEquals(int257(1), stack.popInt257())
+        assertEquals(int257(4), stack.popInt257())
+        assertEquals(int257(3), stack.popInt257())
+    }
+
+    @Test
+    fun testPick() = testFift {
+        interpret("1 2 3")
+        interpret("0 pick")
+        assertEquals(int257(3), stack.popInt257())
+        interpret("1 pick")
+        assertEquals(int257(2), stack.popInt257())
+        interpret("2 pick")
+        assertEquals(int257(1), stack.popInt257())
+        repeat(stack.depth) {
+            stack.pop()
+        }
+    }
+}
