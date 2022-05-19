b1a6fe3f2d592c56549ef54666ed63868b969672
Anton F <imantonmeep@gmail.com>
2022-05-19 17:03:07 +0200

    Address parser/extended toString method (#5)
    * WIP: parse addresses from string, either raw or base64 encoded
    
    * Convert all companion object methods to static, add test
    
    * Add base64url
    
    * Parse base64url encoded addresses, add tests for user-friendly address parsing
    
    * Add toString() method to convert AddrStd into desired address format with flags

--- ton-block/src/commonMain/kotlin/org/ton/block/MsgAddressInt.kt
+++ ton-block/src/commonMain/kotlin/org/ton/block/MsgAddressInt.kt
@@ -6,8 +6,11 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonClassDiscriminator
-import org.ton.crypto.HexByteArraySerializer
-import org.ton.crypto.hex
+import org.ton.bigint.BigInt
+import org.ton.bigint.toBigInt
+import org.ton.crypto.*
+import kotlin.experimental.and
+import kotlin.experimental.or
 
 @OptIn(ExperimentalSerializationApi::class)
 @JsonClassDiscriminator("@type")
@@ -16,10 +19,10 @@ sealed interface MsgAddressInt {
     @SerialName("addr_std")
     @Serializable
     data class AddrStd(
-            val anycast: Anycast?,
-            val workchain_id: Int,
-            @Serializable(HexByteArraySerializer::class)
-            val address: ByteArray
+        val anycast: Anycast?,
+        val workchain_id: Int,
+        @Serializable(HexByteArraySerializer::class)
+        val address: ByteArray
     ) : MsgAddressInt {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
@@ -50,16 +53,106 @@ sealed interface MsgAddressInt {
             append(hex(address))
             append(")")
         }
+
+        fun toString(
+            userFriendly: Boolean = true,
+            urlSafe: Boolean = true,
+            testOnly: Boolean = false,
+            bounceable: Boolean = true
+        ): String {
+            if (userFriendly) {
+                val raw = byteArrayOf(tag(testOnly, bounceable), workchain_id.toByte()) +
+                        address + crc(this, testOnly, bounceable).toShort().toBigInt().toByteArray()
+                if (urlSafe) {
+                    return base64url(raw)
+                } else {
+                    return base64(raw)
+                }
+            } else {
+                return workchain_id.toString() + ":" + hex(address)
+            }
+        }
+
+        companion object {
+            @JvmStatic
+            fun parse(address: String): AddrStd {
+                if (address.contains(':')) {
+                    return parseRaw(address)
+                } else {
+                    return parseUserFriendly(address)
+                }
+            }
+
+            @JvmStatic
+            fun parseRaw(address: String): AddrStd {
+                require(address.contains(':'))
+                // 32 bytes, each represented as 2 characters
+                require(address.substringAfter(':').length == 32 * 2)
+                return AddrStd(
+                    anycast = null,
+                    // toByte() to make sure it fits into 8 bits
+                    workchain_id = address.substringBefore(':').toByte().toInt(),
+                    address = hex(address.substringAfter(':'))
+                )
+            }
+
+            @JvmStatic
+            fun parseUserFriendly(address: String): AddrStd {
+                var raw: ByteArray
+                try {
+                    raw = base64url(address)
+                } catch (E: Exception) {
+                    raw = base64(address)
+                }
+
+                require(raw.size == 36)
+                return AddrStd(
+                    anycast = null,
+                    workchain_id = raw[1].toInt(),
+                    address = raw.sliceArray(2..33)
+                ).apply {
+                    val testOnly = raw[0] and 0x80.toByte() != 0.toByte()
+                    if (testOnly) {
+                        // not 0x80 = 0x7F; here we clean the test only flag
+                        raw[0] = raw[0] and 0x7F.toByte()
+                    }
+
+                    require((raw[0] == 0x11.toByte()) or (raw[0] == 0x51.toByte())) { "unknown address tag" }
+
+                    val bounceable = raw[0] == 0x11.toByte()
+                    require(
+                        (crc(
+                            this,
+                            testOnly,
+                            bounceable
+                        ).toBigInt() == BigInt(raw.sliceArray(34..35)))
+                    ) { "CRC check failed" }
+                }
+            }
+
+            @JvmStatic
+            private fun crc(address: AddrStd, testOnly: Boolean, bounceable: Boolean): Int =
+                crc16(
+                    byteArrayOf(tag(testOnly, bounceable), address.workchain_id.toByte()),
+                    address.address
+                )
+
+            @JvmStatic
+            // Get the tag byte based on set flags
+            private fun tag(testOnly: Boolean, bounceable: Boolean): Byte =
+                (if (testOnly) 0x80.toByte() else 0.toByte()) or
+                        (if (bounceable) 0x11.toByte() else 0x51.toByte())
+        }
     }
 
     @SerialName("addr_var")
     @Serializable
     data class AddrVar(
-            val anycast: Anycast?,
-            val addr_len: Int,
-            val workchain_id: Int,
-            @Serializable(HexByteArraySerializer::class)
-            val address: ByteArray
+        val anycast: Anycast?,
+        val addr_len: Int,
+        val workchain_id: Int,
+        @Serializable(HexByteArraySerializer::class)
+        val address: ByteArray
     ) : MsgAddressInt {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
---
+++ ton-block/src/commonTest/kotlin/org/ton/block/MsgAddressIntTest.kt
@@ -0,0 +1,181 @@
+package org.ton.block
+
+import org.ton.crypto.hex
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MsgAddressIntTest {
+    @Test
+    fun `parse raw addresses`() {
+        val addr1 =
+            MsgAddressInt.AddrStd.parseRaw("-1:3333333333333333333333333333333333333333333333333333333333333333")
+        assertEquals(-1, addr1.workchain_id)
+        assertEquals("3333333333333333333333333333333333333333333333333333333333333333", hex(addr1.address))
+
+        val addr2 =
+            MsgAddressInt.AddrStd.parseRaw("0:83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8")
+        assertEquals(0, addr2.workchain_id)
+        assertEquals("83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8", hex(addr2.address))
+
+        val addr3 =
+            MsgAddressInt.AddrStd.parseRaw("-1:dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5")
+        assertEquals(-1, addr3.workchain_id)
+        assertEquals("dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5", hex(addr3.address))
+    }
+
+    @Test
+    fun `parse user-friendly base64(url) addresses`() {
+        val bounceableAddr1 =
+            MsgAddressInt.AddrStd.parseUserFriendly("Ef8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzM0vF")
+        assertEquals(-1, bounceableAddr1.workchain_id)
+        assertEquals("3333333333333333333333333333333333333333333333333333333333333333", hex(bounceableAddr1.address))
+
+        val nonBounceableAddr1 =
+            MsgAddressInt.AddrStd.parseUserFriendly("Uf8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMxYA")
+        assertEquals(-1, nonBounceableAddr1.workchain_id)
+        assertEquals(
+            "3333333333333333333333333333333333333333333333333333333333333333",
+            hex(nonBounceableAddr1.address)
+        )
+
+        assertEquals(bounceableAddr1, nonBounceableAddr1)
+
+        val bounceableAddr2 =
+            MsgAddressInt.AddrStd.parseUserFriendly("EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N")
+        assertEquals(0, bounceableAddr2.workchain_id)
+        assertEquals("83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8", hex(bounceableAddr2.address))
+
+        val nonBounceableAddr2 =
+            MsgAddressInt.AddrStd.parseUserFriendly("UQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqEBI")
+        assertEquals(0, nonBounceableAddr2.workchain_id)
+        assertEquals(
+            "83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8",
+            hex(nonBounceableAddr2.address)
+        )
+
+        assertEquals(bounceableAddr2, nonBounceableAddr2)
+
+
+        val bounceableAddr3 =
+            MsgAddressInt.AddrStd.parseUserFriendly("Ef_dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN-xWdr")
+        assertEquals(-1, bounceableAddr3.workchain_id)
+        assertEquals("dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5", hex(bounceableAddr3.address))
+
+        val nonBounceableAddr3 =
+            MsgAddressInt.AddrStd.parseUserFriendly("Uf_dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN-xTqu")
+        assertEquals(-1, nonBounceableAddr3.workchain_id)
+        assertEquals(
+            "dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5",
+            hex(nonBounceableAddr3.address)
+        )
+
+        assertEquals(bounceableAddr3, nonBounceableAddr3)
+
+        val bounceableAddr4 =
+            MsgAddressInt.AddrStd.parseUserFriendly("Ef/dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN+xWdr")
+        assertEquals(-1, bounceableAddr4.workchain_id)
+        assertEquals("dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5", hex(bounceableAddr4.address))
+
+        val nonBounceableAddr4 =
+            MsgAddressInt.AddrStd.parseUserFriendly("Uf/dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN+xTqu")
+        assertEquals(-1, nonBounceableAddr4.workchain_id)
+        assertEquals(
+            "dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5",
+            hex(nonBounceableAddr4.address)
+        )
+
+        assertEquals(bounceableAddr4, nonBounceableAddr4)
+    }
+
+    @Test
+    fun `address to raw string`() {
+        val addr1 =
+            MsgAddressInt.AddrStd(null, -1, hex("3333333333333333333333333333333333333333333333333333333333333333"))
+        assertEquals(
+            "-1:3333333333333333333333333333333333333333333333333333333333333333",
+            addr1.toString(userFriendly = false)
+        )
+        assertEquals(
+            "-1:3333333333333333333333333333333333333333333333333333333333333333",
+            addr1.toString(userFriendly = false, testOnly = true)
+        )
+        assertEquals(
+            "-1:3333333333333333333333333333333333333333333333333333333333333333",
+            addr1.toString(userFriendly = false, testOnly = true, bounceable = true)
+        )
+        assertEquals(
+            "-1:3333333333333333333333333333333333333333333333333333333333333333",
+            addr1.toString(userFriendly = false, bounceable = true)
+        )
+
+        val addr2 =
+            MsgAddressInt.AddrStd(null, 0, hex("83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8"))
+        assertEquals(
+            "0:83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8",
+            addr2.toString(userFriendly = false)
+        )
+
+        val addr3 =
+            MsgAddressInt.AddrStd(null, -1, hex("dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5"))
+        assertEquals(
+            "-1:dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5",
+            addr3.toString(userFriendly = false)
+        )
+    }
+
+    @Test
+    fun `address to user-friendly base64(url) string`() {
+        val addr1 =
+            MsgAddressInt.AddrStd(null, -1, hex("3333333333333333333333333333333333333333333333333333333333333333"))
+        assertEquals(
+            "Ef8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzM0vF",
+            addr1.toString(userFriendly = true, urlSafe = true, testOnly = false, bounceable = true)
+        )
+        assertEquals(
+            "Ef8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzM0vF",
+            addr1.toString(userFriendly = true, urlSafe = false, testOnly = false, bounceable = true)
+        )
+        assertEquals(
+            "Uf8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMxYA",
+            addr1.toString(userFriendly = true, urlSafe = true, testOnly = false, bounceable = false)
+        )
+        assertEquals(
+            "Uf8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMxYA",
+            addr1.toString(userFriendly = true, urlSafe = false, testOnly = false, bounceable = false)
+        )
+
+        val addr2 =
+            MsgAddressInt.AddrStd(null, 0, hex("83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8"))
+        assertEquals(
+            "EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N",
+            addr2.toString(bounceable = true)
+        )
+        assertEquals(
+            "UQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqEBI",
+            addr2.toString(bounceable = false)
+        )
+
+        val addr3 =
+            MsgAddressInt.AddrStd(null, -1, hex("dd24c4a1f2b88f8b7053513b5cc6c5a31bc44b2a72dcb4d8c0338af0f0d37ec5"))
+        assertEquals(
+            "Ef_dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN-xWdr",
+            addr3.toString(urlSafe = true, testOnly = false, bounceable = true)
+        )
+        assertEquals(
+            "Ef/dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN+xWdr",
+            addr3.toString(urlSafe = false, testOnly = false, bounceable = true)
+        )
+        assertEquals(
+            "Uf_dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN-xTqu",
+            addr3.toString(urlSafe = true, testOnly = false, bounceable = false)
+        )
+        assertEquals(
+            "Uf/dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN+xTqu",
+            addr3.toString(urlSafe = false, testOnly = false, bounceable = false)
+        )
+        assertEquals(
+            "kf_dJMSh8riPi3BTUTtcxsWjG8RLKnLctNjAM4rw8NN-xdzh",
+            addr3.toString(urlSafe = true, testOnly = true, bounceable = true)
+        )
+    }
+}
