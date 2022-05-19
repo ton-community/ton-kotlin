d0ae97e9f69faacd9e7c2845bbe72cd2afc2f641
Anton Fediushin <imantonmeep@gmail.com>
2022-05-19 11:51:30 +0200

    Add base64url

--- ton-crypto/src/commonMain/kotlin/org/ton/crypto/base64.kt
+++ ton-crypto/src/commonMain/kotlin/org/ton/crypto/base64.kt
@@ -3,3 +3,7 @@ package org.ton.crypto
 expect fun base64(string: String): ByteArray
 
 expect fun base64(byteArray: ByteArray): String
+
+expect fun base64url(string: String): ByteArray
+
+expect fun base64url(byteArray: ByteArray): String
