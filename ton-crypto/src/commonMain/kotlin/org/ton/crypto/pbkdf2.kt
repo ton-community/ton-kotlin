f702fc6790996caf3ba54d05820b0e454904e762
Anton F <imantonmeep@gmail.com>
2022-05-23 19:25:55 +0200

    Fix overloads

--- ton-crypto/src/commonMain/kotlin/org/ton/crypto/pbkdf2.kt
+++ ton-crypto/src/commonMain/kotlin/org/ton/crypto/pbkdf2.kt
@@ -2,4 +2,4 @@ package org.ton.crypto
 
 expect fun pbkdf2_sha512(key: CharArray, salt: ByteArray, iterations: Int): ByteArray
 
-fun pbkdf2_sha512(key: ByteArray, salt: String, iterations: Int) = pbkdf2_sha512(key, salt.toByteArray(), iterations)
+fun pbkdf2_sha512(key: ByteArray, salt: String, iterations: Int): ByteArray = pbkdf2_sha512(key, salt.toByteArray(), iterations)
