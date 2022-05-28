ef672792f23e15d63194a16f1fa0325d4c163c09
Anton F <imantonmeep@gmail.com>
2022-05-28 14:14:14 +0200

    Add basic sanity tests for crc16

---
+++ ton-crypto/src/commonTest/kotlin/org/ton/crypto/Crc16Test.kt
@@ -0,0 +1,30 @@
+package org.ton.crypto
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Crc16Test {
+    @Test
+    fun `sanity tests`() {
+        assertEquals(
+            0x0000,
+            crc16("")
+        )
+        assertEquals(
+            0x31C3,
+            crc16("123456789")
+        )
+        assertEquals(
+            0x9DD6,
+            crc16("abc")
+        )
+        assertEquals(
+            0x3994,
+            crc16("ABC")
+        )
+        assertEquals(
+            0x21E3,
+            crc16("This is a string")
+        )
+    }
+}
