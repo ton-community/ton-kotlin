e4a9834f65ec2d30594d1b2bdbd15810a8576ba8
Andrey Pfau <andreypfau@ton.org>
2022-06-02 17:54:53 +0300

    move back classes in MsgAddressInt & MsgAddressExt

--- ton-block/src/commonMain/kotlin/org/ton/block/MsgAddressExt.kt
+++ ton-block/src/commonMain/kotlin/org/ton/block/MsgAddressExt.kt
@@ -2,12 +2,15 @@
 
 package org.ton.block
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonClassDiscriminator
 import org.ton.bitstring.BitString
+import org.ton.bitstring.toBitString
+import org.ton.cell.CellBuilder
+import org.ton.cell.CellSlice
 import org.ton.tlb.TlbCombinator
 import org.ton.tlb.TlbConstructor
-import org.ton.tlb.exception.UnknownTlbConstructorException
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun MsgAddressExt(externalAddress: BitString? = null): MsgAddressExt = MsgAddressExt.of(externalAddress)
@@ -29,22 +32,82 @@ sealed interface MsgAddressExt : MsgAddress {
         fun of(externalAddress: ByteArray): MsgAddressExt = MsgAddressExtOrdinary(externalAddress)
 
         @JvmStatic
-        fun tlbCodec(): TlbCombinator<MsgAddressExt> = MsgAddressExtTlbCombinator()
+        fun tlbCodec(): TlbCombinator<MsgAddressExt> = MsgAddressExtTlbCombinator
+    }
+
+    private object MsgAddressExtTlbCombinator : TlbCombinator<MsgAddressExt>() {
+        private val addrNoneConstructor by lazy { MsgAddressExtNone.tlbCodec() }
+        private val addrExternConstructor by lazy { MsgAddressExtOrdinary.tlbCodec() }
+
+        override val constructors: List<TlbConstructor<out MsgAddressExt>> by lazy {
+            listOf(addrNoneConstructor, addrExternConstructor)
+        }
+
+        override fun getConstructor(value: MsgAddressExt): TlbConstructor<out MsgAddressExt> = when (value) {
+            is MsgAddressExtNone -> addrNoneConstructor
+            is MsgAddressExtOrdinary -> addrExternConstructor
+        }
     }
 }
 
-private class MsgAddressExtTlbCombinator : TlbCombinator<MsgAddressExt>() {
-    private val addrNoneConstructor by lazy { MsgAddressExtNone.tlbCodec() }
-    private val addrExternConstructor by lazy { MsgAddressExtOrdinary.tlbCodec() }
-
-    override val constructors: List<TlbConstructor<out MsgAddressExt>> by lazy {
-        listOf(addrNoneConstructor, addrExternConstructor)
+@SerialName("addr_extern")
+@Serializable
+data class MsgAddressExtOrdinary(
+    val len: Int,
+    @SerialName("external_address")
+    val externalAddress: BitString
+) : MsgAddressExt {
+    init {
+        require(externalAddress.size == len) { "externalAddress.size expected: $len actual: ${externalAddress.size}" }
     }
 
-    @Suppress("REDUNDANT_ELSE_IN_WHEN") // kotlin compiler bug
-    override fun getConstructor(value: MsgAddressExt): TlbConstructor<out MsgAddressExt> = when (value) {
-        is MsgAddressExtNone -> addrNoneConstructor
-        is MsgAddressExtOrdinary -> addrExternConstructor
-        else -> throw UnknownTlbConstructorException()
+    constructor(externalAddress: ByteArray) : this(externalAddress.toBitString())
+    constructor(externalAddress: BitString) : this(externalAddress.size, externalAddress)
+
+    companion object {
+        @JvmStatic
+        fun tlbCodec(): TlbConstructor<MsgAddressExtOrdinary> = AddrExternTlbConstructor
+    }
+
+    private object AddrExternTlbConstructor : TlbConstructor<MsgAddressExtOrdinary>(
+        schema = "addr_extern\$01 len:(## 9) external_address:(bits len) = MsgAddressExt;"
+    ) {
+        override fun storeTlb(
+            cellBuilder: CellBuilder, value: MsgAddressExtOrdinary
+        ) = cellBuilder {
+            storeUInt(value.len, 9)
+            storeBits(value.externalAddress)
+        }
+
+        override fun loadTlb(
+            cellSlice: CellSlice
+        ): MsgAddressExtOrdinary = cellSlice {
+            val len = loadUInt(9).toInt()
+            val externalAddress = loadBitString(len)
+            MsgAddressExtOrdinary(len, externalAddress)
+        }
+    }
+}
+
+@SerialName("addr_none")
+@Serializable
+object MsgAddressExtNone : MsgAddressExt {
+    override fun toString(): String = "addr_none"
+
+    @JvmStatic
+    fun tlbCodec(): TlbConstructor<MsgAddressExtNone> = AddrNoneTlbConstructor
+
+    private object AddrNoneTlbConstructor : TlbConstructor<MsgAddressExtNone>(
+        schema = "addr_none\$00 = MsgAddressExt;"
+    ) {
+        override fun storeTlb(
+            cellBuilder: CellBuilder,
+            value: MsgAddressExtNone
+        ) {
+        }
+
+        override fun loadTlb(cellSlice: CellSlice): MsgAddressExtNone {
+            return MsgAddressExtNone
+        }
     }
 }
--- ton-block/src/commonMain/kotlin/org/ton/block/MsgAddressInt.kt
+++ ton-block/src/commonMain/kotlin/org/ton/block/MsgAddressInt.kt
@@ -1,12 +1,26 @@
 package org.ton.block
 
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonClassDiscriminator
+import org.ton.bigint.toBigInt
+import org.ton.bitstring.BitString
+import org.ton.bitstring.toBitString
+import org.ton.cell.CellBuilder
+import org.ton.cell.CellSlice
+import org.ton.crypto.base64
+import org.ton.crypto.base64url
+import org.ton.crypto.crc16
+import org.ton.crypto.hex
 import org.ton.tlb.TlbCombinator
 import org.ton.tlb.TlbConstructor
+import org.ton.tlb.loadTlb
+import org.ton.tlb.storeTlb
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
+import kotlin.experimental.and
+import kotlin.experimental.or
 
 @OptIn(ExperimentalSerializationApi::class)
 @JsonClassDiscriminator("@type")
@@ -16,7 +30,7 @@ sealed interface MsgAddressInt : MsgAddress {
 
     companion object {
         @JvmStatic
-        fun tlbCodec(): TlbCombinator<MsgAddressInt> = MsgAddressIntTlbCombinator()
+        fun tlbCodec(): TlbCombinator<MsgAddressInt> = MsgAddressIntTlbCombinator
 
         @JvmStatic
         fun toString(
@@ -49,23 +63,236 @@ sealed interface MsgAddressInt : MsgAddress {
             }
         }
     }
+
+    private object MsgAddressIntTlbCombinator : TlbCombinator<MsgAddressInt>() {
+        private val addrStdConstructor by lazy {
+            MsgAddressIntStd.tlbCodec()
+        }
+        private val addrVarConstructor by lazy {
+            MsgAddressIntVar.tlbCodec()
+        }
+
+        override val constructors: List<TlbConstructor<out MsgAddressInt>> by lazy {
+            listOf(addrStdConstructor, addrVarConstructor)
+        }
+
+        override fun getConstructor(value: MsgAddressInt): TlbConstructor<out MsgAddressInt> = when (value) {
+            is MsgAddressIntStd -> addrStdConstructor
+            is MsgAddressIntVar -> addrVarConstructor
+        }
+    }
 }
 
-internal class MsgAddressIntTlbCombinator : TlbCombinator<MsgAddressInt>() {
-    private val addrStdConstructor by lazy {
-        MsgAddressIntStd.tlbCodec()
-    }
-    private val addrVarConstructor by lazy {
-        MsgAddressIntVar.tlbCodec()
+
+@SerialName("addr_std")
+@Serializable
+data class MsgAddressIntStd(
+    val anycast: Maybe<Anycast>,
+    @SerialName("workchain_id")
+    override val workchainId: Int,
+    val address: BitString
+) : MsgAddressInt {
+    init {
+        require(address.size == 256) { "address.size expected: 256 actual: ${address.size}" }
     }
 
-    override val constructors: List<TlbConstructor<out MsgAddressInt>> by lazy {
-        listOf(addrStdConstructor, addrVarConstructor)
+    constructor(workchainId: Int, address: BitString) : this(null, workchainId, address)
+    constructor(workchainId: Int, address: ByteArray) : this(null, workchainId, address)
+    constructor(anycast: Anycast?, workchainId: Int, address: ByteArray) : this(
+        anycast.toMaybe(),
+        workchainId,
+        address.toBitString()
+    )
+
+    constructor(anycast: Anycast?, workchainId: Int, address: BitString) : this(
+        anycast.toMaybe(),
+        workchainId,
+        address
+    )
+
+    fun toString(
+        userFriendly: Boolean = true,
+        urlSafe: Boolean = true,
+        testOnly: Boolean = false,
+        bounceable: Boolean = true
+    ): String = toString(this, userFriendly, urlSafe, testOnly, bounceable)
+
+    companion object {
+        @JvmStatic
+        fun tlbCodec(): TlbConstructor<MsgAddressIntStd> = AddrStdTlbConstructor
+
+        @JvmStatic
+        fun toString(
+            address: MsgAddressIntStd,
+            userFriendly: Boolean = true,
+            urlSafe: Boolean = true,
+            testOnly: Boolean = false,
+            bounceable: Boolean = true
+        ): String {
+            return if (userFriendly) {
+                val raw = byteArrayOf(tag(testOnly, bounceable), address.workchainId.toByte()) +
+                        address.address.toByteArray() + crc(address, testOnly, bounceable).toShort().toBigInt()
+                    .toByteArray()
+                if (urlSafe) {
+                    base64url(raw)
+                } else {
+                    base64(raw)
+                }
+            } else {
+                address.workchainId.toString() + ":" + hex(address.address.toByteArray())
+            }
+        }
+
+        @JvmStatic
+        fun parse(address: String): MsgAddressIntStd {
+            return if (address.contains(':')) {
+                parseRaw(address)
+            } else {
+                parseUserFriendly(address)
+            }
+        }
+
+        @JvmStatic
+        fun parseRaw(address: String): MsgAddressIntStd {
+            require(address.contains(':'))
+            // 32 bytes, each represented as 2 characters
+            require(address.substringAfter(':').length == 32 * 2)
+            return MsgAddressIntStd(
+                // toByte() to make sure it fits into 8 bits
+                workchainId = address.substringBefore(':').toByte().toInt(),
+                address = hex(address.substringAfter(':'))
+            )
+        }
+
+        @JvmStatic
+        fun parseUserFriendly(address: String): MsgAddressIntStd {
+            val raw = try {
+                base64url(address)
+            } catch (E: Exception) {
+                base64(address)
+            }
+
+            require(raw.size == 36)
+            return MsgAddressIntStd(
+                workchainId = raw[1].toInt(),
+                address = raw.sliceArray(2..33)
+            ).apply {
+                val testOnly = raw[0] and 0x80.toByte() != 0.toByte()
+                if (testOnly) {
+                    // not 0x80 = 0x7F; here we clean the test only flag
+                    raw[0] = raw[0] and 0x7F.toByte()
+                }
+
+                check((raw[0] == 0x11.toByte()) or (raw[0] == 0x51.toByte())) { "unknown address tag" }
+
+                val bounceable = raw[0] == 0x11.toByte()
+                check(
+                    (crc(
+                        this,
+                        testOnly,
+                        bounceable
+                    ) == raw[34].toUByte().toInt() * 256 + raw[35].toUByte().toInt())
+                ) { "CRC check failed" }
+            }
+        }
+
+        private fun crc(address: MsgAddressIntStd, testOnly: Boolean, bounceable: Boolean): Int =
+            crc16(
+                byteArrayOf(tag(testOnly, bounceable), address.workchainId.toByte()),
+                address.address.toByteArray()
+            )
+
+        // Get the tag byte based on set flags
+        private fun tag(testOnly: Boolean, bounceable: Boolean): Byte =
+            (if (testOnly) 0x80.toByte() else 0.toByte()) or
+                    (if (bounceable) 0x11.toByte() else 0x51.toByte())
     }
 
-    override fun getConstructor(value: MsgAddressInt): TlbConstructor<out MsgAddressInt> = when (value) {
-        is MsgAddressIntStd -> addrStdConstructor
-        is MsgAddressIntVar -> addrVarConstructor
+    private object AddrStdTlbConstructor : TlbConstructor<MsgAddressIntStd>(
+        schema = "addr_std\$10 anycast:(Maybe Anycast) workchain_id:int8 address:bits256 = MsgAddressInt;"
+    ) {
+        private val maybeAnycastCodec by lazy {
+            Maybe.tlbCodec(Anycast.tlbCodec())
+        }
+
+        override fun storeTlb(
+            cellBuilder: CellBuilder,
+            value: MsgAddressIntStd
+        ) = cellBuilder {
+            storeTlb(maybeAnycastCodec, value.anycast)
+            storeInt(value.workchainId, 8)
+            storeBits(value.address)
+        }
+
+        override fun loadTlb(
+            cellSlice: CellSlice
+        ): MsgAddressIntStd = cellSlice {
+            val anycast = loadTlb(maybeAnycastCodec)
+            val workchainId = loadInt(8).toInt()
+            val address = loadBitString(256)
+            MsgAddressIntStd(anycast, workchainId, address)
+        }
     }
 }
 
+@SerialName("addr_var")
+@Serializable
+data class MsgAddressIntVar(
+    val anycast: Maybe<Anycast>,
+    val addrLen: Int,
+    override val workchainId: Int,
+    val address: BitString
+) : MsgAddressInt {
+    init {
+        require(address.size == addrLen) { "address.size expected: $addrLen actual: ${address.size}" }
+    }
+
+    constructor(workchainId: Int, address: ByteArray) : this(null, workchainId, address)
+    constructor(workchainId: Int, address: BitString) : this(null, workchainId, address)
+    constructor(anycast: Anycast?, workchainId: Int, address: BitString) : this(
+        anycast.toMaybe(),
+        address.size,
+        workchainId,
+        address
+    )
+
+    constructor(anycast: Anycast?, workchainId: Int, address: ByteArray) : this(
+        anycast.toMaybe(),
+        address.size,
+        workchainId,
+        address.toBitString()
+    )
+
+    companion object {
+        @JvmStatic
+        fun tlbCodec(): TlbConstructor<MsgAddressIntVar> = AddrVarTlbConstructor
+    }
+
+    private object AddrVarTlbConstructor : TlbConstructor<MsgAddressIntVar>(
+        schema = "addr_var\$11 anycast:(Maybe Anycast) addr_len:(## 9) workchain_id:int32 address:(bits addr_len) = MsgAddressInt;"
+    ) {
+        private val maybeAnycastCodec by lazy {
+            Maybe.tlbCodec(Anycast.tlbCodec())
+        }
+
+        override fun storeTlb(
+            cellBuilder: CellBuilder,
+            value: MsgAddressIntVar
+        ) = cellBuilder {
+            storeTlb(maybeAnycastCodec, value.anycast)
+            storeUInt(value.addrLen, 9)
+            storeInt(value.workchainId, 32)
+            storeBits(value.address)
+        }
+
+        override fun loadTlb(
+            cellSlice: CellSlice
+        ): MsgAddressIntVar = cellSlice {
+            val anycast = loadTlb(maybeAnycastCodec)
+            val addrLen = loadUInt(9).toInt()
+            val workchainId = loadInt(32).toInt()
+            val address = loadBitString(addrLen)
+            MsgAddressIntVar(anycast, addrLen, workchainId, address)
+        }
+    }
+}
