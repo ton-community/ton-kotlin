3df587f3d78872c12df3097ec9ba5f675bc012e4
Andrey Pfau <andreypfau@ton.org>
2022-07-14 06:12:42 +0300

    WIP

--- ton-crypto/src/commonMain/kotlin/org/ton/crypto/aes/EncryptorAes.kt
+++ ton-crypto/src/commonMain/kotlin/org/ton/crypto/aes/EncryptorAes.kt
@@ -1,6 +1,7 @@
 package org.ton.crypto.aes
 
 import org.ton.crypto.Encryptor
+import org.ton.crypto.encodeHex
 import org.ton.crypto.sha256
 
 class EncryptorAes(
@@ -8,6 +9,8 @@ class EncryptorAes(
 ) : Encryptor {
     override fun encrypt(data: ByteArray): ByteArray {
         val digest = sha256(data)
+        println("params: ${data.encodeHex()}")
+        println("digest: ${digest.encodeHex()}")
         val key = ByteArray(32)
         secret.copyInto(key, destinationOffset = 0, startIndex = 0, endIndex = 16)
         digest.copyInto(key, destinationOffset = 16, startIndex = 16, endIndex = 32)
@@ -16,10 +19,17 @@ class EncryptorAes(
         digest.copyInto(ctr, destinationOffset = 0, startIndex = 0, endIndex = 4)
         secret.copyInto(ctr, destinationOffset = 4, startIndex = 20, endIndex = 32)
 
+        println("key: ${key.encodeHex()}")
+        println("ctr: ${ctr.encodeHex()}")
+
         val cipher = AesCtr(key, ctr)
         val encryptedData = cipher.encrypt(data)
 
-        return digest + encryptedData
+        println("encrypted aes: ${encryptedData.encodeHex()}")
+
+        return (digest + encryptedData).also {
+            println("result aes: ${it.encodeHex()}")
+        }
     }
 
     override fun verify(message: ByteArray, signature: ByteArray): Boolean =
--- ton-crypto/src/commonMain/kotlin/org/ton/crypto/ed25519/EncryptorEd25519.kt
+++ ton-crypto/src/commonMain/kotlin/org/ton/crypto/ed25519/EncryptorEd25519.kt
@@ -3,16 +3,25 @@ package org.ton.crypto.ed25519
 import org.ton.crypto.Encryptor
 import org.ton.crypto.X25519
 import org.ton.crypto.aes.EncryptorAes
+import org.ton.crypto.encodeHex
+import org.ton.crypto.hex
 
 class EncryptorEd25519(
     private val publicKey: ByteArray
 ) : Encryptor {
     override fun encrypt(data: ByteArray): ByteArray {
-        val privateKey = Ed25519.privateKey()
+//        val privateKey = Ed25519.privateKey()
+        println("other public: ${hex(publicKey)}")
+        val privateKey = ByteArray(32)
+        val localPublic = Ed25519.publicKey(privateKey)
+        println("local public: ${hex(localPublic)}")
         val secret = X25519.sharedKey(privateKey, Ed25519.convertToX25519(publicKey))
+        println("secret: ${secret.encodeHex()}")
         val aes = EncryptorAes(secret)
         val encryptedData = aes.encrypt(data)
-        return publicKey + encryptedData
+        return (localPublic + encryptedData).also {
+            println("result ed25519: ${it.encodeHex()}")
+        }
     }
 
     override fun verify(message: ByteArray, signature: ByteArray): Boolean =
