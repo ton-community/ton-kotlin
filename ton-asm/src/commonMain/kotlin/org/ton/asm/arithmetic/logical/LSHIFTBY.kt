6a3389c2234b674e53c7adfc28034a15198bfa0d
Andrey Pfau <andreypfau@ton.org>
2022-07-24 14:04:17 +0300

    LSHIFTBY

---
+++ ton-asm/src/commonMain/kotlin/org/ton/asm/arithmetic/logical/LSHIFTBY.kt
@@ -0,0 +1,28 @@
+package org.ton.asm.arithmetic.logical
+
+import org.ton.cell.CellBuilder
+import org.ton.cell.CellSlice
+import org.ton.tlb.TlbConstructor
+import org.ton.tlb.providers.TlbConstructorProvider
+
+data class LSHIFTBY(
+    val c: Int
+) {
+    override fun toString(): String = "$c LSHIFT#"
+
+    companion object : TlbConstructorProvider<LSHIFTBY> by LSHIFTBYTlbConstructor
+}
+
+private object LSHIFTBYTlbConstructor : TlbConstructor<LSHIFTBY>(
+    schema = "asm_lshiftby#aa c:uint8 = LSHIFTBY;",
+    type = LSHIFTBY::class
+) {
+    override fun storeTlb(cellBuilder: CellBuilder, value: LSHIFTBY) {
+        cellBuilder.storeUInt(value.c, 8)
+    }
+
+    override fun loadTlb(cellSlice: CellSlice): LSHIFTBY {
+        val c = cellSlice.loadUInt(8).toInt()
+        return LSHIFTBY(c)
+    }
+}
