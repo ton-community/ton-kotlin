89a74f9a0f0bed9b41f4f7a68e936734ef8b6aba
MrKamen <admin@agemagic.ru>
2022-09-19 16:27:37 +0300

    formatting fix 1

--- ton-kotlin-asm/src/commonMain/kotlin/org/ton/asm/codepage/SETCP.kt
+++ ton-kotlin-asm/src/commonMain/kotlin/org/ton/asm/codepage/SETCP.kt
@@ -1,10 +1,10 @@
 package org.ton.asm.codepage
 
 import org.ton.asm.AsmInstruction
+import org.ton.bigint.toUByte
 import org.ton.cell.CellBuilder
 import org.ton.cell.CellSlice
 import org.ton.tlb.TlbConstructor
-import org.ton.bigint.*
 import org.ton.tlb.providers.TlbConstructorProvider
 
 data class SETCP(
@@ -28,4 +28,3 @@ private object SETCPTlbConstructor : TlbConstructor<SETCP>(
         return SETCP(nn)
     }
 }
-
