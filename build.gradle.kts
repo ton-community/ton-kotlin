d916e2fc1a49a92d1f80f881c1574792efbcebe1
Andrey Pfau <andreypfau@ton.org>
2023-02-09 02:41:09 +0300

    0.2.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val isCI = System.getenv("CI") == "true"
 
 allprojects {
     group = "org.ton"
-    version = "0.2.14"
+    version = "0.2.15"
 
     apply(plugin = "kotlin-multiplatform")
     apply(plugin = "kotlinx-serialization")
