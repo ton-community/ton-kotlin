4cdfeeff9849e48d304bcf3e9b301547c500b251
Andrey Pfau <andreypfau@ton.org>
2023-02-13 22:49:02 +0300

    highload wallet interfaces

--- ton-kotlin-contract/src/commonMain/kotlin/org/ton/contract/wallet/HighLoadWalletV2Contract.kt
+++ ton-kotlin-contract/src/commonMain/kotlin/org/ton/contract/wallet/HighLoadWalletV2Contract.kt
@@ -1,6 +1,5 @@
 package org.ton.contract.wallet
 
-import io.ktor.utils.io.bits.*
 import org.ton.api.pub.PublicKeyEd25519
 import org.ton.bitstring.BitString
 import org.ton.bitstring.Bits256
@@ -9,7 +8,6 @@ import org.ton.block.MsgAddressInt
 import org.ton.cell.Cell
 import org.ton.cell.CellBuilder
 import org.ton.cell.CellSlice
-import org.ton.cell.buildCell
 import org.ton.hashmap.HashMapE
 import org.ton.tlb.TlbCodec
 import org.ton.tlb.TlbConstructor
@@ -33,19 +31,22 @@ public class HighLoadWalletV2Contract(
 
     public fun getPublicKey(): PublicKeyEd25519 = PublicKeyEd25519(requireNotNull(loadData()).publicKey)
 
-    public class Data(
-        public val subWalletId: Int,
-        public val lastCleaned: Long,
-        public val publicKey: Bits256,
+    public interface Data {
+        public val subWalletId: Int
+        public val lastCleaned: Long
+        public val publicKey: Bits256
         public val oldQueries: HashMapE<WalletMessage<Cell>>
-    ) {
-        public companion object : TlbConstructorProvider<Data> by HighLoadWalletV2DataTlbConstructor
+
+        public companion object : TlbConstructorProvider<Data> by HighLoadWalletV2DataTlbConstructor {
+            @JvmStatic
+            override fun tlbConstructor(): TlbConstructor<Data> = HighLoadWalletV2DataTlbConstructor
+        }
     }
 
-    public class Query<X : Any>(
-        public val signature: BitString,
+    public interface Query<X : Any> {
+        public val signature: BitString
         public val payload: QueryPayload<X>
-    ) {
+
         public companion object {
             @JvmStatic
             public fun <X : Any> tlbConstructor(x: TlbCodec<X>): TlbConstructor<Query<X>> =
@@ -53,56 +54,46 @@ public class HighLoadWalletV2Contract(
         }
     }
 
-    public data class QueryPayload<X : Any>(
-        public val subWalletId: Int,
-        public val queryId: Long,
+    public interface QueryPayload<X : Any> {
+        public val subWalletId: Int
+        public val queryId: Long
         public val msgs: HashMapE<WalletMessage<X>>
-    ) {
+
         public companion object {
             @JvmStatic
-            public fun <X : Any> of(
-                subWalletId: Int,
-                queryId: Long,
-                msgs: HashMapE<WalletMessage<X>>
-            ): QueryPayload<X> = QueryPayload(subWalletId, queryId, msgs)
-
-            @JvmStatic
-            public fun <X : Any> of(
-                subWalletId: Int,
-                x: TlbCodec<X>,
-                msgs: HashMapE<WalletMessage<X>>,
-            ): QueryPayload<X> {
-                val codec = HashMapE.tlbCodec(16, WalletMessage.tlbCodec(x))
-                val queryId = buildCell {
-                    storeTlb(codec, msgs)
-                }.hash().useMemory(0, 64) {
-                    it.loadLongAt(0)
-                }
-                return of(subWalletId, queryId, msgs)
-            }
-
-            @JvmStatic
-            public fun <X : Any> of(
-                subWalletId: Int,
-                x: TlbCodec<X>,
-                msgs: Iterable<WalletMessage<X>>
-            ): QueryPayload<X> {
-                var hashMap = HashMapE.empty<WalletMessage<X>>()
-                msgs.forEachIndexed { index, walletMessage ->
-                    val key = buildCell {
-                        storeInt(index, 16)
-                    }.bits
-                    hashMap = hashMap.set(key, walletMessage)
-                }
-                return of(subWalletId, x, hashMap)
-            }
-
-            @JvmStatic
-            public fun <X:Any> tlbConstructor(x: TlbCodec<X>): TlbConstructor<QueryPayload<X>> =
+            public fun <X : Any> tlbConstructor(x: TlbCodec<X>): TlbConstructor<QueryPayload<X>> =
                 HighLoadWalletV2QueryPayloadTlbConstructor(x)
         }
     }
+
+    public companion object {
+        @JvmStatic
+        public fun data(
+            subWalletId: Int,
+            lastCleaned: Long,
+            publicKey: Bits256,
+            oldQueries: HashMapE<WalletMessage<Cell>>
+        ): Data = HighLoadWalletV2DataImpl(subWalletId, lastCleaned, publicKey, oldQueries)
+
+        @JvmStatic
+        public fun <X : Any> query(
+            signature: BitString,
+            payload: QueryPayload<X>
+        ): Query<X> = HighLoadWalletV2QueryImpl(signature, payload)
+
+        @JvmStatic
+        public fun <X : Any> queryPayload(
+            subWalletId: Int,
+            queryId: Long,
+            msgs: HashMapE<WalletMessage<X>>
+        ): QueryPayload<X> = HighLoadWalletV2QueryPayloadImpl(subWalletId, queryId, msgs)
+    }
 }
+
+private data class HighLoadWalletV2QueryImpl<X : Any>(
+    override val signature: BitString,
+    override val payload: HighLoadWalletV2Contract.QueryPayload<X>
+) : HighLoadWalletV2Contract.Query<X>
 
 private class HighLoadWalletV2QueryTlbConstructor<X : Any>(
     val x: TlbCodec<X>
@@ -112,7 +103,7 @@ private class HighLoadWalletV2QueryTlbConstructor<X : Any>(
     override fun loadTlb(cellSlice: CellSlice): HighLoadWalletV2Contract.Query<X> {
         val signature = cellSlice.loadBits(512)
         val payload = cellSlice.loadTlb(HighLoadWalletV2Contract.QueryPayload.tlbConstructor(x))
-        return HighLoadWalletV2Contract.Query(signature, payload)
+        return HighLoadWalletV2QueryImpl(signature, payload)
     }
 
     override fun storeTlb(cellBuilder: CellBuilder, value: HighLoadWalletV2Contract.Query<X>) {
@@ -120,6 +111,12 @@ private class HighLoadWalletV2QueryTlbConstructor<X : Any>(
         HighLoadWalletV2Contract.QueryPayload.tlbConstructor(x).storeTlb(cellBuilder, value.payload)
     }
 }
+
+private data class HighLoadWalletV2QueryPayloadImpl<X : Any>(
+    override val subWalletId: Int,
+    override val queryId: Long,
+    override val msgs: HashMapE<WalletMessage<X>>
+) : HighLoadWalletV2Contract.QueryPayload<X>
 
 private class HighLoadWalletV2QueryPayloadTlbConstructor<X : Any>(
     x: TlbCodec<X>
@@ -132,7 +129,7 @@ private class HighLoadWalletV2QueryPayloadTlbConstructor<X : Any>(
         val subWalletId = cellSlice.loadInt(32).toInt()
         val queryId = cellSlice.loadInt(64).toLong()
         val msgs = cellSlice.loadTlb(msgs)
-        return HighLoadWalletV2Contract.QueryPayload(subWalletId, queryId, msgs)
+        return HighLoadWalletV2QueryPayloadImpl(subWalletId, queryId, msgs)
     }
 
     override fun storeTlb(cellBuilder: CellBuilder, value: HighLoadWalletV2Contract.QueryPayload<X>) {
@@ -142,6 +139,13 @@ private class HighLoadWalletV2QueryPayloadTlbConstructor<X : Any>(
     }
 }
 
+private data class HighLoadWalletV2DataImpl(
+    override val subWalletId: Int,
+    override val lastCleaned: Long,
+    override val publicKey: Bits256,
+    override val oldQueries: HashMapE<WalletMessage<Cell>>
+) : HighLoadWalletV2Contract.Data
+
 private object HighLoadWalletV2DataTlbConstructor : TlbConstructor<HighLoadWalletV2Contract.Data>(
     schema = "highload_wallet_v2_data#_ sub_wallet_id:uint32 last_cleaned:uint64 public_key:bits256 old_queries:HighLoadWalletV2Queries = HighLoadWalletV2Data"
 ) {
@@ -150,7 +154,7 @@ private object HighLoadWalletV2DataTlbConstructor : TlbConstructor<HighLoadWalle
         val lastCleaned = cellSlice.loadInt(64).toLong()
         val publicKey = cellSlice.loadBits256()
         val oldQueries = cellSlice.loadTlb(HighLoadWalletV2QueriesTlbConstructor)
-        return HighLoadWalletV2Contract.Data(subWalletId, lastCleaned, publicKey, oldQueries)
+        return HighLoadWalletV2DataImpl(subWalletId, lastCleaned, publicKey, oldQueries)
     }
 
     override fun storeTlb(cellBuilder: CellBuilder, value: HighLoadWalletV2Contract.Data) {
