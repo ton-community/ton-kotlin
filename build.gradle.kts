b0ba9374733108333136fda5d0d1148364ff7630
Andrey Pfau <andreypfau@ton.org>
2023-12-12 16:33:27 +0700

    fix auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ nexusPublishing {
         sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("OSSRH_USERNAME"))
+            password.set(System.getenv("OSSRH_PASSWORD"))
         }
     }
 }
